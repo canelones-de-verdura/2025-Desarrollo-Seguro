40be0e621ba0f24758a495a15e85878de1dc6639
Eze <eze.gonzalez@vera.com.uy>
2025-09-24 20:28:33 -0300

    2 - Credenciales embebidas

--- services/backend/src/middleware/auth.middleware.ts
+++ services/backend/src/middleware/auth.middleware.ts
@@ -1,26 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwtUtils from '../utils/jwt';
 
 interface JwtPayload {
-  id: string;
+    id: string;
 }
 
 export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader?.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Missing or invalid token' });
-  }
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Missing or invalid token' });
+    }
 
-  const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, "secreto_super_seguro");
-    (req as any).user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ message: 'Invalid token' });
-  }
+    try {
+        const decoded = jwtUtils.verifyToken(token);
+        (req as any).user = decoded;
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: 'Invalid token' });
+    }
 };
 
 export default authenticateJWT;
--- services/backend/src/utils/jwt.ts
+++ services/backend/src/utils/jwt.ts
@@ -1,15 +1,18 @@
 import jwt from 'jsonwebtoken';
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const generateToken = (userId: string) => {
+    console.log(JWT_SECRET)
   return jwt.sign(
     { id: userId }, 
-    "secreto_super_seguro", 
+    JWT_SECRET, 
     { expiresIn: '1h' }
   );
 };
 
 const verifyToken = (token: string) => {
-  return jwt.verify(token, "secreto_super_seguro");
+  return jwt.verify(token, JWT_SECRET);
 };
 
 export default {
